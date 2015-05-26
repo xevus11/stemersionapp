ec63a7e88c064cd2d4f9ea29cb8bf186940ba6f8
Technetium99 <orlando.marinella@gmail.com>
2015-05-26 13:03:07 -0400

    upload of changes 3
    attempt to communicate full app data 3

---
+++ app/build/generated/source/buildConfig/debug/comxevus11stemersionapp/httpsgithub/three_dimensionalraumschachchess/BuildConfig.java
@@ -0,0 +1,13 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package comxevus11stemersionapp.httpsgithub.three_dimensionalraumschachchess;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "comxevus11stemersionapp.httpsgithub.three_dimensionalraumschachchess";
+  public static final String BUILD_TYPE = "debug";
+  public static final String FLAVOR = "";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+}
